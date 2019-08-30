aea8e8ba8abc87b905f7d3bc93ebb7d11261d8ef
HallvardMM <49036938+HallvardMM@users.noreply.github.com>
2019-08-30 11:21:13 +0200

    Add files via upload
    player classes

---
+++ Rock Scissors Paper/player.py
@@ -0,0 +1,29 @@
+"""
+player abstract class
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Player(ABC):
+    """Abstract Player class"""
+    points = 0.0
+
+
+    @abstractmethod
+    def get_class_name(self):
+        """Returns the Class name"""
+
+
+    @abstractmethod
+    def action(self, opponent):
+        """Returns the action"""
+
+    @abstractmethod
+    def save_result(self, opponent, opponent_action):
+        """Saves the result from a match"""
+
+    @abstractmethod
+    def add_points(self, value):
+        """add points"""
+        self.points += value
